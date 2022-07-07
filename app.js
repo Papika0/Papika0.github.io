45fc38eaa99c3454dec8f20b82edc531d18439f2
Papika0 <d_papismedovi@cu.edu.ge>
2022-07-07 16:17:07 +0400

    validator fixed (TO DO CSS)

--- app.js
+++ app.js
@@ -1,57 +1,76 @@
 const steps = Array.from(document.querySelectorAll("form .step"));
-const nextBtn = document.querySelectorAll("form .GetS-Button,.next-btn");
+const nextBtn = document.querySelectorAll("form .GetS-Button");
 const form = document.querySelector("form");
 const prevBtn = document.querySelectorAll("form .back-btn");
 
-let nameG = document.getElementById("Name");
+let fName = document.getElementById("Name");
 let email = document.getElementById("Email");
 let pnum = document.getElementById("Pnumber");
 let date = document.getElementById("Date");
 
-nameG.addEventListener("input", validateInput);
-email.addEventListener("input", validateInput);
-pnum.addEventListener("input", validateInput);
-date.addEventListener("input", validateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-function validateInput(e) {
-  if (e.target.id == "Name") {
-    if (e.target.value.length >= 2) {
-      e.target.classList.add("valid");
-      e.target.classList.remove("invalid");
-    } else {
-      e.target.classList.add("invalid");
-      e.target.classList.remove("valid");
-    }
+  if (checkInputs()) {
+    changeStep("next");
+  }
+});
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const fNameValue = fName.value.trim();
+  const emailValue = email.value.trim();
+  const pnumValue = pnum.value.trim();
+  const dateValue = date.value.trim();
+
+  let check = 0;
+
+  if (fNameValue.length < 2) {
+    setErrorFor(fName, "Name cannot be blank");
+    console.log("Wrong name");
+  } else {
+    check++;
+    setSuccessFor(fName);
   }
   let emailVal = "@redberry.ge";
-  if (e.target.id == "Email") {
-    if (e.target.value.includes(emailVal)) {
-      e.target.classList.add("valid");
-      e.target.classList.remove("invalid");
-    } else {
-      e.target.classList.add("invalid");
-      e.target.classList.remove("valid");
-    }
+  if (emailValue.includes(emailVal)) {
+    setSuccessFor(email);
+    check++;
+  } else {
+    setErrorFor(email, "Not a valid email");
+    console.log("wrong email");
   }
-  if (e.target.id == "Pnumber") {
-    if (e.target.value.length == 9) {
-      e.target.classList.add("valid");
-      e.target.classList.remove("invalid");
-    } else {
-      e.target.classList.add("invalid");
-      e.target.classList.remove("valid");
-    }
+
+  if (pnumValue.length == 9) {
+    setSuccessFor(pnum);
+    check++;
+  } else {
+    setErrorFor(pnum, "pnum cannot be blank");
+    console.log("wrong  phone");
   }
-  if (e.target.id == "Date") {
-    console.log(!e.target.date);
-    if (!e.target.date) {
-      e.target.classList.add("valid");
-      e.target.classList.remove("invalid");
-    } else {
-      e.target.classList.add("invalid");
-      e.target.classList.remove("valid");
-    }
+
+  if (!dateValue) {
+    setErrorFor(date, "Date can't be blank");
+    console.log("wrong date");
+  } else {
+    setSuccessFor(date);
+    check++;
   }
+  if (check == 4) {
+    return true;
+  } else {
+    console.log("need to be Validated");
+  }
+}
+
+function setErrorFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "input-0 error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "input-0 success";
 }
 
 nextBtn.forEach((button) => {
