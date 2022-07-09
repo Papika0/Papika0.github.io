8e37934f8d1344de0f529c294be3e0e648a17429
Papika0 <d_papismedovi@cu.edu.ge>
2022-07-10 02:41:03 +0400

    Selection , radio button fixed working css rocket.

--- selection.js
+++ selection.js
@@ -4,6 +4,24 @@ var options = document.getElementsByClassName("options");
 var list = document.getElementById("list");
 var arrowIcon = document.getElementById("selector-arrow");
 
+var selectField1 = document.getElementById("selectField1");
+var selectText1 = document.getElementById("selectText1");
+var options1 = document.getElementsByClassName("options1");
+var list1 = document.getElementById("list1");
+
+selectField1.onclick = function () {
+  list1.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
+
+for (option of options1) {
+  option.onclick = function () {
+    selectText1.innerHTML = this.textContent;
+    list1.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+  };
+}
+
 selectField.onclick = function () {
   list.classList.toggle("hide");
   arrowIcon.classList.toggle("rotate");
